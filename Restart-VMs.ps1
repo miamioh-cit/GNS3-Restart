d389beffb56016760baad2f8d6f8f1626d5e83dc
Wes Taylor <150176347+WTaylor8miami@users.noreply.github.com>
2024-04-12 11:56:59 -0400

    Update Restart-VMs.ps1

--- Restart-VMs.ps1
+++ Restart-VMs.ps1
@@ -5,7 +5,10 @@ param(
 )
 
 # Ignore invalid or self-signed SSL certificates
-Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false -Scope User
+
+# Connect to the vCenter Server using the provided credentials
+Connect-VIServer -Server $vCenterServer -User $vCenterUser -Password $vCenterPass
 
 # List of VMs to restart
 $vmList = @(
@@ -13,17 +16,18 @@ $vmList = @(
     "284-11", "284-12", "284-13", "284-14", "284-15", "284-16", "284-17", "386-00", "386-01", "358-01"
 )
 
-# Connect to the vCenter Server using the provided credentials
-Connect-VIServer -Server $vCenterServer -User $vCenterUser -Password $vCenterPass
-
 # Loop through each VM name and restart the VM if found
 foreach ($vmName in $vmList) {
-    $vm = Get-VM -Name $vmName
-    if ($vm -ne $null) {
-        Write-Host "Restarting VM: $vmName"
-        Restart-VMGuest -VM $vm -Confirm:$false
-    } else {
-        Write-Host "VM not found: $vmName"
+    try {
+        $vm = Get-VM -Name $vmName
+        if ($vm -ne $null) {
+            Write-Host "Restarting VM: $vmName"
+            Restart-VMGuest -VM $vm -Confirm:$false
+        } else {
+            Write-Host "VM not found: $vmName"
+        }
+    } catch {
+        Write-Host "Error processing VM: $vmName. Error: $_"
     }
 }
 
